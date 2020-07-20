191dd364e6e98c6e70ef1d1fea4e48faecc07bab
Dinuka <drpiyadigama@gmail.com>
2020-07-20 17:47:49 +0530

    Implemented displaying of Mini-cart item count with update of cart

--- front-end/src/components/MiniCart.tsx
+++ front-end/src/components/MiniCart.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Badge } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
+import { ReduxState } from "../store/reducers";
+import { useSelector } from "react-redux";
 
 const MiniCart = () => {
+  let cart: any = useSelector((state: ReduxState) => state.cart); // get entire cart object saved in Redux state
+  let [itemQuantity, setItemQuantity] = useState(0);
+
+  useEffect(() => {
+    setItemQuantity(cart.cartItems.length);
+  }, [cart.cartItems.length]);
+
   return (
     <>
-{/* give hover effect to this section */}
+      {/* give hover effect to this section */}
 
-    {/* get the number of items from redux state - Have a useEffect hook to change this whenever the redux state changes??*/}
-      <Badge count={1}>
+      <Badge count={itemQuantity}>
         <ShoppingCartOutlined style={{ fontSize: "28px", color: "#08c" }} />
       </Badge>
     </>
