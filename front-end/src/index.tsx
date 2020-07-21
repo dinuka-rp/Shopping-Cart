dd68572a439c7907f4c5006609aa55c054eff742
Dinuka <drpiyadigama@gmail.com>
2020-07-21 09:56:29 +0530

    Wp: Started implementing Redux-persist for cart

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -5,8 +5,10 @@ import "antd/dist/antd.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
-import {rootReducer} from "./store/reducers";
+import { rootReducer } from "./store/reducers";
 import { Provider } from "react-redux";
+import { persistStore} from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 // initialize redux store
 const store = createStore(
@@ -14,11 +16,14 @@ const store = createStore(
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION__() // redux devtools chrome extension
 );
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
