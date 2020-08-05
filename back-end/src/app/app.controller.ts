9869bb1f293e2c4d0feb1083bd4aa0d1dde442bc
dinuka-rp <drpiyadigama@gmail.com>
2020-08-05 14:04:10 +0530

    Used LocalAuthGuard instead of directly using AuthGuard

--- back-end/src/app/app.controller.ts
+++ back-end/src/app/app.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;
