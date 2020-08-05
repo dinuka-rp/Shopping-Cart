1725ac4ac58fad6ccef7121d3fa90b173c667248
dinuka-rp <drpiyadigama@gmail.com>
2020-08-05 14:45:04 +0530

    Implemented generating & returning jwt token upon sign in.

--- back-end/src/app/app.controller.ts
+++ back-end/src/app/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { AuthService } from '../auth/auth.service';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    // return req.user;
+    return this.authService.login(req.user);
   }
 
   // @Get()
